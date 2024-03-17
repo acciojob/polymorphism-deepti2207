81f52c1ecbf3218a1f7c90465ed13cfb0d22b013
Deepti rai <144608021+deepti2207@users.noreply.github.com>
2024-03-17 18:05:34 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,37 @@
 package com.driver;
 
 public class Main {
+  public static void main(String[] args) {
+        // Task 2: Create object of Product in Main function called p
+        Product p = new Product();
+        
+        // Task 3: Call the method product(int x, int y) from Main using Product class object p
+        int result1 = p.product(2, 3);
+        System.out.println("Product of 2 and 3 is: " + result1);
+        
+        // Task 4: Call the overloaded method product(int x, int y, int z) from Main using Product class object p
+        int result2 = p.product(2, 3, 4);
+        System.out.println("Product of 2, 3, and 4 is: " + result2);
+        
+        // Task 5: Call the overloaded method product(double x, double y) from Main using Product class object p
+        double result3 = p.product(2.5, 3.5);
+        System.out.println("Product of 2.5 and 3.5 is: " + result3);
+    }
+}
 
+class Product {
+    // Task 3: Create a method with the following definition: public int product(int x, int y) {}
+    public int product(int x, int y) {
+        return x * y;
+    }
+    
+    // Task 4: Create an overloaded method with the following definition: public int product(int x, int y, int z) {}
+    public int product(int x, int y, int z) {
+        return x * y * z;
+    }
+    
+    // Task 5: Create an overloaded method with the following definition: public double product(double x, double y) {}
+    public double product(double x, double y) {
+        return x * y;
+    }
 }
